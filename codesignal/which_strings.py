c6de42eabc5fb07514a247e8b77eb96ef5f2f319
muhyasin89 <muhyasin89@gmail.com>
2019-12-17 13:17:50 +0700

    [UPDATE]update new interview_practice

---
+++ codesignal/which_strings.py
@@ -0,0 +1,24 @@
+a = 'ab'
+b = "ab"
+c = ""ab""
+d = ''ab''
+e = '''ab'''
+f = """ab"""
+
+if type(a) is String:
+    print("a  is True")
+
+if type(b) is String:
+    print("b  is True")
+
+if type(c) is String:
+    print("c  is True")
+
+if type(d) is String:
+    print("d  is True")
+
+if type(e) is String:
+    print("e  is True")
+
+if type(f) is String:
+    print("f  is True")
